45ce3bffa9aac1fd2cf6dcf2ee34d34fdccccabb
pcn-physics <58860457+pcn-physics@users.noreply.github.com>
2020-03-28 23:54:16 -0500

    Update factors.r

--- factors.r
+++ factors.r
@@ -7,7 +7,7 @@ so start learning how to create, subset, and compare them now.
 
 '''
 Exercise
-What's a factor and why would you use it?
+What\'s a factor and why would you use it?
 In this chapter you dive into the wonderful world of factors.
 
 The term factor refers to a statistical data type used to store categorical variables. 
@@ -34,13 +34,13 @@ Assign to variable theory the value "factors for categorical variables".
 
 '''
 Exercise
-What's a factor and why would you use it? (2)
+What\'s a factor and why would you use it? (2)
 To create factors in R, you make use of the function factor(). 
 First thing that you have to do is create a vector that contains all the observations that belong to a limited number of categories. 
 For example, sex_vector contains the sex of 5 different individuals:
 
 sex_vector <- c("Male","Female","Female","Male","Male")
-It is clear that there are two categories, or in R-terms 'factor levels', at work here: "Male" and "Female".
+It is clear that there are two categories, or in R-terms \'factor levels\', at work here: "Male" and "Female".
 
 The function factor() will encode the vector as a factor:
 
@@ -69,11 +69,11 @@ factor in the console. This will open up a help page. Continue to the next exerc
 
 '''
 Exercise
-What's a factor and why would you use it? (3)
+What\'s a factor and why would you use it? (3)
 There are two types of categorical variables: a nominal categorical variable and an ordinal categorical variable.
 
 A nominal variable is a categorical variable without an implied order. 
-This means that it is impossible to say that 'one is worth more than the other'. 
+This means that it is impossible to say that \'one is worth more than the other\'. 
 For example, think of the categorical variable animals_vector with the categories "Elephant", "Giraffe", "Donkey" and "Horse". 
 Here, it is impossible to say that one stands above or below the other. (Note that some of you might disagree ;-) ).
 
@@ -83,7 +83,7 @@ Here it is obvious that "Medium" stands above "Low", and "High" stands above "Me
 
 Instructions
 100 XP
-Click 'Submit Answer' to check how R constructs and prints nominal and ordinal variables. Do not worry if you do not understand all the code just yet, we will get to that.
+Click \'Submit Answer\' to check how R constructs and prints nominal and ordinal variables. Do not worry if you do not understand all the code just yet, we will get to that.
 '''
 
 '''SOLN:
@@ -121,8 +121,8 @@ but it can introduce confusion when analyzing the data. At that point,
 you will often want to change the factor levels to "Male" and "Female" instead of "M" and "F" for clarity.
 
 Watch out: the order with which you assign the levels is important. 
-If you type levels(factor_survey_vector), you'll see that it outputs [1] "F" "M". 
-If you don't specify the levels of the factor when creating the vector, R will automatically assign them alphabetically. 
+If you type levels(factor_survey_vector), you\'ll see that it outputs [1] "F" "M". 
+If you don\'t specify the levels of the factor when creating the vector, R will automatically assign them alphabetically. 
 To correctly map "F" to "Female" and "M" to "Male", the levels should be set to c("Female", "Male"), in this order.
 
 Instructions
@@ -192,7 +192,7 @@ In factor_survey_vector you have a factor with two levels: "Male" and "Female". 
 
 Instructions
 100 XP
-Read the code in the editor and click 'Submit Answer' to test if male is greater than (>) female.
+Read the code in the editor and click \'Submit Answer\' to test if male is greater than (>) female.
 '''
 
 '''SOLN:
@@ -207,13 +207,13 @@ Read the code in the editor and click 'Submit Answer' to test if male is greater
 > # Female
 > female <- factor_survey_vector[2]
 > 
-> # Battle of the sexes: Male 'larger' than female?
+> # Battle of the sexes: Male \'larger\' than female?
 > male > female
 Warning message: '>' not meaningful for factors
 [1] NA
 
-#How interesting! By default, R returns NA when you try to compare values in a factor, since the idea doesn't make sense. 
-#Next you'll learn about ordered factors, where more meaningful comparisons are possible.
+#How interesting! By default, R returns NA when you try to compare values in a factor, since the idea doesn\'t make sense. 
+#Next you\'ll learn about ordered factors, where more meaningful comparisons are possible.
 '''
 
 '''
@@ -247,5 +247,73 @@ No need to specify these are factors yet.
 > speed_vector <- c("medium", "slow","slow", "medium", "fast")
 '''
 
+
 '''
+Exercise
+Ordered factors (2)
+speed_vector should be converted to an ordinal factor since its categories have a natural ordering. 
+By default, the function factor() transforms speed_vector into an unordered factor. 
+To create an ordered factor, you have to add two additional arguments: ordered and levels.
+
+factor(some_vector,
+       ordered = TRUE,
+       levels = c("lev1", "lev2" ...))
+By setting the argument ordered to TRUE in the function factor(), you indicate that the factor is ordered. 
+With the argument levels you give the values of the factor in the correct order.
+
+Instructions
+100 XP
+From speed_vector, create an ordered factor vector: factor_speed_vector. 
+Set ordered to TRUE, and set levels to c("slow", "medium", "fast").
+'''
+
+'''SOLN
+> # Create speed_vector
+> speed_vector <- c("medium", "slow", "slow", "medium", "fast")
+> 
+> # Convert speed_vector to ordered factor vector
+> factor_speed_vector <- factor(speed_vector, ordered = TRUE,
+  levels = c("slow", "medium", "fast"))
+> 
+> # Print factor_speed_vector
+> factor_speed_vector
+[1] medium slow   slow   medium fast  
+Levels: slow < medium < fast
+> summary(factor_speed_vector)
+  slow medium   fast 
+     2      2      1
+'''
+
+'''
+Exercise
+Comparing ordered factors
+Having a bad day at work, \'data analyst number two\' enters your office and starts complaining that 
+\'data analyst number five\' is slowing down the entire project. Since you know that \'data analyst number two\' 
+has the reputation of being a smarty-pants, you first decide to check if his statement is true.
+
+The fact that factor_speed_vector is now ordered enables us to compare different elements (the data analysts in this case). 
+You can simply do this by using the well-known operators.
+
+Instructions
+100 XP
+Use [2] to select from factor_speed_vector the factor value for the second data analyst. Store it as da2.
+Use [5] to select the factor_speed_vector factor value for the fifth data analyst. Store it as da5.
+Check if da2 is greater than da5; simply print out the result. 
+Remember that you can use the > operator to check whether one element is larger than the other.
+'''
+
+'''SOLN:
+> # Create factor_speed_vector
+> speed_vector <- c("medium", "slow", "slow", "medium", "fast")
+> factor_speed_vector <- factor(speed_vector, ordered = TRUE, levels = c("slow", "medium", "fast"))
+> 
+> # Factor value for second data analyst
+> da2 <- factor_speed_vector[2]
+> 
+> # Factor value for fifth data analyst
+> da5 <- factor_speed_vector[5]
+> 
+> # Is data analyst 2 faster than data analyst 5?
+> da2 > da5
+[1] FALSE
 '''
